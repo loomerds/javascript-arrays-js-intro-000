b0ed7b37c363a868b22619e456b614d019ef3cfd
Doug Loomer <loomerds@gmail.com>
2019-05-12 12:59:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, element) {
+  return [...arr, element];
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element);
+  return arr;
+}
