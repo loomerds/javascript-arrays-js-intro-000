53c38ddb98c8d7e65ce371a687d41c20a1881802
Doug Loomer <loomerds@gmail.com>
2019-05-12 14:09:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,8 @@ function removeElementFromBeginningOfArray(arr) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(arr) {
-  return arr.pop()
+  arr.pop();
+  return arr;
 }
 
 function removeElementFromEndOfArray(arr) {
